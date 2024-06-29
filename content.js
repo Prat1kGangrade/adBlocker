048e07a6759aa4f4d4633d347911ac29f6858626
pratikgangrade <gangradeprateek420@gmail.com>
2024-06-29 20:51:46 +0530

    changed something

--- content.js
+++ content.js
@@ -6,16 +6,5 @@ const removeAds = () => {
     // Skip overlay ads
     const overlayAds = document.querySelectorAll('.ytp-ad-overlay-container');
     overlayAds.forEach(ad => ad.remove());
-  
-    // Remove sidebar ads
-    const sidebarAds = document.querySelectorAll('.ytd-promoted-sparkles-text-search-renderer, .ytd-promoted-video-renderer');
-    sidebarAds.forEach(ad => ad.remove());
-  };
-  
-  // Initial ad removal
-  removeAds();
-  
-  // Observe for new ad elements being added dynamically
-  const observer = new MutationObserver(removeAds);
-  observer.observe(document.body, { childList: true, subtree: true });
+}
   
